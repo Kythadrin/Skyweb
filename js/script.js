3962f3b9e98c867a5571e4fbad64a718ba0e7eb6
Kythadrin <vadimsiric@gmail.com>
2022-01-30 23:30:43 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,46 +1,12 @@
-$(window).resize(function() {
-    if ($(window).width() > 767) {
-        $(".menu_list").css("height", "100%");
-    } else {
-        $(".menu_list").css({
-            "height" : "0",
-            "transition" : "0s",
-        });
-    }
-});
-
 $(document).ready(function() {
     $(".menu").click(function() {
-        let content = $("content");
-        let menu = $(".menu_list");
-
-        menu.toggleClass('visible');
-        menu.toggleClass('hidden');
-        content.toggleClass('navbar-opened');
-        content.toggleClass('navbar-closed');
-
-        if (menu.css("height") == "200px") {
-            menu.css({
-                "height" : "0",
-                "border-bottom" : "0",
-            });
-            content.css("margin-top", "0");
-        } else {
-            menu.css({
-                "height" : "200px",
-                "border-bottom" : "1px",
-                "border-bottom-color" : "lightgrey",
-                "border-bottom-style" : "solid",
-            });
-            content.css("margin-top", "200px");
-        }  
+        $(".menu_list").toggleClass('visible');
+        $("content").toggleClass('navbar-opened');
     });
 
     $(".accordion").each( function() {
         $(this).on("click", function() {
-            $(this).toggleClass('deactive');
             $(this).toggleClass('active');
-            $(this).next().toggleClass('hidden');
             $(this).next().toggleClass('visible');
         });
     });
